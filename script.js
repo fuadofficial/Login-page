c76749040f0c3c3156ba6195695dfed8c3cf85bd
fuad <ahmadfuadoorakam@gmail.com>
2024-04-21 19:43:49 +0530

    copleted js file codes

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+const validation=()=>{
+    var name = document.getElementById('name')
+    var email = document.getElementById('email')
+    var password = document.getElementById('password')
+    var submit = document.getElementById('submit')
 
-    document.getElementById("loginForm").addEventListener('submit', (event) => {
-        event.preventDefault();
-        window.open("./login.html", "_blank");
-    })
-
+    if(name.value === ""){
+        alert('Please Enter YOur Name')
+        document.loginForm.name.focus()
+        return false
+    }else if(email.value === ""){
+        alert('Please Enter YOur email')
+        document.loginForm.email.focus()
+        return false
+    }else if(name.password === ""){
+        alert('Please Add YOur Password')
+        document.loginForm.password.focus()
+        return false
+    }
+    return true
+}
